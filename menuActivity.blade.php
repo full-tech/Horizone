411404ede50cefac5f9887e87db2b013236324dd
Thi Tuong Vien Nguyen <105220885+full-tech@users.noreply.github.com>
2023-12-04 21:24:44 +0100

    changer menu activity
    Ajouter workshop

---
+++ menuActivity.blade.php
@@ -0,0 +1,18 @@
+<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+  <li><h5 class="dropdownTitle" style="color:cornflowerblue">@lang('dic.Activities') @lang('dic.of') @lang('dic.Team')</h5>
+  </li> 
+  <li><a class="dropdown-item" href="{{URL::to('/MissionFebruary2020')}}">&diams; @lang('dic.Conference') @lang('dic.February') 2020 </a>
+  </li>
+   <!-- <li><a class="dropdown-item" href="#">&diams; @lang('dic.Workshop') Vienna @lang('dic.May') 2022 </a></li> -->
+  <li><a class="dropdown-item" href="{{URL::to('/MissionDOT2022')}}">&diams; DOT - @lang('dic.September') 2022 </a>
+  </li>
+ <li><a class="dropdown-item" href="{{URL::to('/WorkshopMar2023')}}">&diams; @lang('dic.Workshop') -  @lang('dic.March') 2023 </a>
+  </li>
+  <li><a class="dropdown-item" href="{{URL::to('/WorkshopMar2023')}}">&diams; Book Launch.Tocharian A Dictionary - October 2023 </a>
+  </li>
+  <li><div class="dropdown-divider"></div></li>
+  <li><h5 class="dropdownTitle" style="color:cornflowerblue">@lang('dic.Conferences'), @lang('dic.Seminars'), @lang('dic.Symposia') @lang('dic.and') @lang('dic.Missions') </h5>
+  </li> 
+  <li><a class="dropdown-item" href="{{URL::to('/conference')}}">&diams; @lang('dic.Conferences'),  @lang('dic.Seminars'), @lang('dic.Symposia')  @lang('dic.and') @lang('dic.Missions')</a>
+  </li>
+</ul>
